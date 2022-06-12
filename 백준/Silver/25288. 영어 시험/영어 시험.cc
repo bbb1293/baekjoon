4c6634e172fab925371417ccee835f36323053fa
superpanty <bbb7931293@gist.ac.kr>
2022-06-13 00:49:08 +0900

    [Silver IV] Title: 영어 시험, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25288. 영어 시험/영어 시험.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+char s[27];
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    scanf("%d", &n);
+    scanf("%s", s);
+
+    while(n--) {
+        printf("%s",s);
+    }
+
+    return 0;
+}
