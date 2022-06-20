ef39e349bc03e4266eefcd371e265bf1988358f0
superpanty <bbb7931293@gist.ac.kr>
2022-06-20 11:50:25 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    printf(
+        "         ,r'\"7\n\
+r`-_   ,'  ,/\n\
+ \\. \". L_r'\n\
+   `~\\/\n\
+      |\n\
+      |");
+
+    return 0;
+}
