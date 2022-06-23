63f9a6d08521a2e74ea98097bbd149b44b98449f
superpanty <bbb7931293@gist.ac.kr>
2022-06-24 02:01:47 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 96 ms, Memory: 10928 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int cnt = 1;
+int n, m, r;
+int order[100001];
+vector<int> edges[100001];
+
+void dfs(int cur) {
+    order[cur] = cnt++;
+
+    for (auto v : edges[cur]) {
+        if (!order[v]) {
+            dfs(v);
+        }
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m >> r;
+
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+        edges[u].push_back(v);
+        edges[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        sort(edges[i].begin(), edges[i].end(), [](int &a, int &b) {
+            return a > b;
+        });
+    }
+
+    dfs(r);
+
+    for (int i = 1; i <= n; i++) {
+        cout << order[i] << '\n';
+    }
+
+    return 0;
+}
