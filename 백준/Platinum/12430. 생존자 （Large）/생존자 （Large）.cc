0281a5376d55d2d5b3399487b660e332f6a69e37
superpanty <bbb7931293@gist.ac.kr>
2022-07-04 19:13:29 +0900

    [Platinum V] Title: 생존자 (Large), Time: 6724 ms, Memory: 2132 KB -BaekjoonHub

--- 백준/Platinum/12430. 생존자 （Large）/생존자 （Large）.cc
+++ 백준/Platinum/12430. 생존자 （Large）/생존자 （Large）.cc
@@ -10,8 +10,8 @@ struct PS {
 
 const int MAXN = 1e3, MAXM = 1e5 + 1e3;
 int n;
-PS ps[MAXN + 1];
-bool possible[MAXN + 1][MAXM + 1];
+PS ps[MAXN];
+bool possible[MAXM + 1];
 
 int main() {
     // freopen("input.txt", "r", stdin);
@@ -21,28 +21,26 @@ int main() {
 
     for (int test_case = 1; test_case <= t; test_case++) {
         scanf("%d", &n);
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             scanf("%d %d", &ps[i].p, &ps[i].s);
         }
-        sort(ps + 1, ps + (n + 1));
+        sort(ps, ps + n);
 
-        possible[0][0] = true;
+        memset(possible, 0, sizeof(possible));
+        possible[0] = true;
 
-        int cur_limit = 0, limit = ps[n].p + ps[n].s;
-        for (int i = 1; i <= n; i++) {
+        int cur_limit = 0, limit = ps[n - 1].p + ps[n - 1].s;
+        for (int i = 0; i < n; i++) {
             cur_limit = min(cur_limit + ps[i].s, limit);
-            for (int j = 0; j <= cur_limit; j++) {
-                possible[i][j] = possible[i - 1][j];
-
-                if (j - ps[i].s >= 0 && j - ps[i].s <= ps[i].p &&
-                    possible[i - 1][j - ps[i].s]) {
-                    possible[i][j] = true;
+            for (int j = cur_limit; j >= ps[i].s; j--) {
+                if (j - ps[i].s <= ps[i].p && possible[j - ps[i].s]) {
+                    possible[j] = true;
                 }
             }
         }
 
         for (int j = cur_limit; j >= 0; j--) {
-            if (possible[n][j]) {
+            if (possible[j]) {
                 printf("Case #%d: %d\n", test_case, j);
                 break;
             }
