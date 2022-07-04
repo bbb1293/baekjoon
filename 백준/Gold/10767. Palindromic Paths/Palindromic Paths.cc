c0a5d95a8ae5e923c79856e0c1ddffdce61d32ea
superpanty <bbb7931293@gist.ac.kr>
2022-07-04 18:17:34 +0900

    [Gold IV] Title: Palindromic Paths, Time: 148 ms, Memory: 30672 KB -BaekjoonHub

--- 백준/Gold/10767. Palindromic Paths/Palindromic Paths.cc
+++ 백준/Gold/10767. Palindromic Paths/Palindromic Paths.cc
@@ -6,32 +6,40 @@ int n;
 char grid[18][19];
 set<string> s[18], ans;
 
-void set_set(int y, int x, string cur) {
+void set_set(int y, int x, string &cur) {
     if (y + x == n - 1) {
         s[x].insert(cur);
     } else {
         if (y + 1 < n) {
-            set_set(y + 1, x, cur + grid[y + 1][x]);
+            cur.push_back(grid[y + 1][x]);
+            set_set(y + 1, x, cur);
+            cur.pop_back();
         }
 
         if (x + 1 < n) {
-            set_set(y, x + 1, cur + grid[y][x + 1]);
+            cur.push_back(grid[y][x + 1]);
+            set_set(y, x + 1, cur);
+            cur.pop_back();
         }
     }
 }
 
-void set_set2(int y, int x, string cur) {
+void set_set2(int y, int x, string &cur) {
     if (y + x == n - 1) {
         if (s[x].count(cur)) {
             ans.insert(cur);
         }
     } else {
         if (y - 1 >= 0) {
-            set_set2(y - 1, x, cur + grid[y - 1][x]);
+            cur.push_back(grid[y - 1][x]);
+            set_set2(y - 1, x, cur);
+            cur.pop_back();
         }
 
         if (x - 1 >= 0) {
-            set_set2(y, x - 1, cur + grid[y][x - 1]);
+            cur.push_back(grid[y][x - 1]);
+            set_set2(y, x - 1, cur);
+            cur.pop_back();
         }
     }
 }
@@ -49,8 +57,11 @@ int main() {
         return 0;
     }
 
-    set_set(0, 0, string(1, grid[0][0]));
-    set_set2(n - 1, n - 1, string(1, grid[n - 1][n - 1]));
+    string tmp(1 ,grid[0][0]);
+    set_set(0, 0, tmp);
+
+    tmp = string(1, grid[n - 1][n - 1]);
+    set_set2(n - 1, n - 1, tmp);
 
     printf("%d", ans.size());
 
