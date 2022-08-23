51900e2d62df202d0c7256e38c9ef2ab962c1d29
superpanty <bbb7931293@gist.ac.kr>
2022-08-23 23:55:51 +0900

    [Bronze IV] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    int n;
+    scanf("%d", &n);
+
+    while(n--) {
+        printf("SciComLove\n");
+    }
+
+    return 0;
+}
