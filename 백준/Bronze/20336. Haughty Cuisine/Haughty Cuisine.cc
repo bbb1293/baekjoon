44e29a65f4de314d85ea122702f5783fe4e99ade
superpanty <bbb7931293@gist.ac.kr>
2022-09-19 21:20:03 +0900

    [Bronze II] Title: Haughty Cuisine, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20336. Haughty Cuisine/Haughty Cuisine.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    int k;
+    cin >> k;
+
+    cout << k << "\n";
+    while (k--) {
+        string tmp;
+        cin >> tmp;
+
+        cout << tmp << "\n";
+    }
+
+    return 0;
+}
