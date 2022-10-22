9d3ba624c0f3730db0b051883f799706d5580829
superpanty <bbb7931293@gist.ac.kr>
2022-10-23 00:11:17 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    long long a, b;
+    scanf("%lld %lld", &a, &b);
+
+    printf("%lld", (a + b) * (a - b));
+
+    return 0;
+}
