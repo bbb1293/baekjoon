aea0a4dfbbef5ab38ad2e847ca55298d09e27371
superpanty <bbb7931293@gist.ac.kr>
2022-11-29 16:59:09 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    int a[3];
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    sort(a, a + 3);
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
